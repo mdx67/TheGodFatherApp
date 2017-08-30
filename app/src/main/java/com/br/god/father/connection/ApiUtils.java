ce309438d08308ac583f4fd669dae2af6b13f6c2
DESKTOP-A2SRRIR\zup-matheus <matheus.mdx@gmail.com>
2017-08-30 10:03:58 -0300

    apiUtils atualizada

--- app/src/main/java/com/br/god/father/connection/ApiUtils.java
+++ app/src/main/java/com/br/god/father/connection/ApiUtils.java
@@ -1,8 +1,26 @@
 package com.br.god.father.connection;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ApiUtils {
 
     public static Connection getConnection(String baseUrl) {
-            return RetrofitClient.getClient(baseUrl).create(Connection.class);
+        return RetrofitClient.getClient(baseUrl).create(Connection.class);
+    }
+
+    public static Map<String, String> buildHeaders(String customerId) {
+        Map<String, String> headers = buildHeaders();
+
+        
+        return headers;
+    }
+
+    public static Map<String, String> buildHeaders() {
+        Map<String, String> headers = new HashMap<>();
+
+        headers.put("Content-Type", "application/json");
+
+        return headers;
     }
 }
